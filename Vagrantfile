3a517ddd5ed38284fd2643333c58329487bf6bb4
Darko Gavrilovic <darko@chass.utoronto.ca>
2018-04-19 13:35:09 -0400

    update files

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,7 @@ Vagrant.configure("2") do |config|
   config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
 
   config.vm.provider "virtualbox" do |vb|
-    vb.name = "CentOS Drupal"
+    vb.name = "CentOS 7 Drupal"
 	vb.gui = false
     vb.memory = "1024"
   end
--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Setting up vagrant image"
-yum update >>/tmp/script.log 2>&1
-yum upgrade -y >>/tmp/script.log 2>&1
-pkgs=(httpd mariadb php php-gd php-xml php-mbstring vim tar wget)
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm >>/tmp/setup.log 2>&1
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm >>/tmp/setup.log 2>&1
+yum-config-manager --enable remi-php72 -y >>/tmp/setup.log 2>&1
+yum update >>/tmp/setup.log 2>&1
+pkgs=(httpd mariadb php php-gd php-xml php-mbstring vim tar wget yum-utils)
 for p in ${pkgs[@]}; do yum install -y ${p}; done; 
-systemctl enable httpd -y >>/tmp/script.log 2>&1
-systemctl enable mariadb-server -y >>/tmp/script.log 2>&1
-reboot >>/tmp/script.log 2>&1
+yum upgrade -y >>/tmp/setup.log 2>&1
+systemctl start httpd >>/tmp/setup.log 2>&1
+systemctl start mariadb-server >>/tmp/setup.log 2>&1
+systemctl enable httpd >>/tmp/setup.log 2>&1
+systemctl enable mariadb-server >>/tmp/setup.log 2>&1
+reboot >>/tmp/setup.log 2>&1
